47865741dacf316e8909829a4d94faadc96d882e
tkarombang <tuangkarombang@gmail.com>
2025-08-24 22:59:10 +0800

    Feat: Nabar Flowbite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,13 @@ export default function Home() {
   return (
     <div>
       <NavbarPage />
+
       <main className={styles.main}>
-        <h1 className="text-shadow-lime-600 text-amber-700">Wellcome App User ManagementAPI</h1>
+        {/* Hero Section */}
+        <section className="flex flex-col items-center justify-center py-20">
+          <h1 className="text-4xl font-bold">Frontend User Management API</h1>
+          <p className="mt-4 text-lg text-gray-700 text-center max-w-2xl">Aplikasi sederhana untuk mengelola pengguna. Dibangun dengan Next.js, Tailwind, dan Flowbite.</p>
+        </section>
       </main>
       <footer className={styles.footer}></footer>
     </div>
